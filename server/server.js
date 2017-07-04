4a2ba6e5d570f0bc2e04adf89fab018725d017e0
Edwin Liu <cslasher@gmail.com>
2017-07-04 18:36:28 +0800

    Create user greeting and joining alert

--- server/server.js
+++ server/server.js
@@ -15,17 +15,29 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connection...');
 
+    socket.emit('newMessage', {
+    	from: 'Admin',
+    	text: 'Welcome to the chat app',
+    	createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+    	from: 'Admin',
+    	text: 'New user joined',
+    	createdAt: new Date().getTime()
+    });
+
     socket.on('disconnect', () => {
         console.log('User disconnected...');
     });
 
     socket.on('createMessage', (newMessage) => {
         console.log('Create message:', newMessage);
-        io.emit('newMessage', {
+       	io.emit('newMessage', {
         	from: newMessage.from,
         	text: newMessage.text,
         	createdAt: new Date().getTime()
-        })
+        });
     });
 });
 
